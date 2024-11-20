931ced4401b035edde3b2f5e3ba9d9ae5f40640a
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2024-11-20 22:29:13 +0530

    feat(test): add 'invoke_onec' helper
    We have functions for converting code and statements to S-expressions,
    however they only provide us with the stdout of onec and thus cannot be
    used to make assertions about exit code of onec.
    
    Lets add a lower-level 'invoke_onec' helper that allows specifying args
    for onec directly and also returns the exit code along with the stdout.

--- t/lib.py
+++ t/lib.py
@@ -1,21 +1,28 @@
 import subprocess
 
 
+def invoke_onec(args: list[str], stdin: str) -> tuple[str, int]:
+    proc = subprocess.Popen(
+        ["onec", *args],
+        stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE,
+    )
+
+    proc.stdin.write(stdin.encode())
+    proc.stdin.close()
+
+    exit_code = proc.wait()
+
+    return (proc.stdout.read().decode(), exit_code)
+
+
 def code2sexpr(code: str) -> str:
     """
     Invokes onec for converting `code` into a S-expressions.
     """
 
-    proc = subprocess.Popen(
-        ["onec", "--s-expr"],
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-    )
-
-    proc.stdin.write(f"{code}\n".encode())
-    proc.stdin.close()
-
-    return proc.stdout.read().decode().strip()
+    stdout, _ = invoke_onec(["--s-expr"], f"{code}\n")
+    return stdout.strip()
 
 
 def stmt2sexpr(stmt: str) -> str:
