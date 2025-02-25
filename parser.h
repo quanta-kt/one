8c92e8d5bcb6ac2ba6a814e0758ce6c0ebb4c61a
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2025-02-26 01:12:11 +0530

    docs(parser): document 'parse'
    Let's add a doc comment to the parse() function and describe its
    functionality in detail.
    
    We eventually want to document most if not all the important
    functions/structs this way.

--- parser.h
+++ parser.h
@@ -6,6 +6,26 @@
 #include "alloc.h"
 #include "ast.h"
 
+/**
+ * Parses the source code and builds AST.
+ * The resulting AST nodes are placed into \p{out}.
+ *
+ * Even when parsing fails due to syntax errors at some locations, parse()
+ * *will* return and set the result into \p{out}. The nodes where the
+ * syntax error occurred will however be "synthetic", i.e. crated by
+ * the parser to satisfy completeness of the AST and not actually parsed
+ * from source.
+ *
+ * NOTE: Not all codepaths currently build the "error nodes" and simply
+ *       exit() when an error occurs. The migration is in progress.
+ *
+ * @param allocator Allocator to use for allocating AST nodes.
+ * @param src Source to parse. Does not have to be NULL-terminated.
+ * @param src_len Length of \p{src}.
+ * @param out The pointer where resultant AST nodes are placed.
+ *
+ * @return true if parsing succeeded without any errors, false otherwise.
+ */
 bool parse(
     allocator_t* allocator, char* src, size_t src_len, ast_item_node** out
 );
